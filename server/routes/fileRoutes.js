d27b880aab365a54aba21d8a12c38816f60b9310
Amit Acharya <amitacharya975@gmail.com>
2021-12-22 16:58:31 +0545

    :zap: work in progress

--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,22 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const multer = require("multer");
+const multer = require('multer');
+const path = require('path');
 
 let storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
-  filename:(req,file,cb)=>{}
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()} - ${Math.round(Math.random() * 1e9)}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
+  },
 });
 
-router.post("/files", (req, res) => {
+let upload = multer({
+  dest: storage,
+  limits: { fileSize: 1000000 * 100 },
+}).single('myFile');
+
+router.post('/files', (req, res) => {
+  //validate request
   if (!req.file) {
     return res.status(404).json({
       sucess: false,
-      message: "All Fields are required",
+      message: 'All Fields are required',
     });
   }
-  res.status(200).json({
-    sucess: true,
-    message: "File uploaded sucessfully",
+
+  //store file
+
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({
+        sucess: false,
+        message: err.message || 'Error uploading files',
+      });
+    }
   });
 });
 
