593116b03962b3a7ea50410a8bacfa399cf94481
Amit Acharya <amitacharya975@gmail.com>
2021-12-27 22:25:47 +0545

    :zap: minor fixes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
 
 //.ENV Configuration
 dotenv.config();
 
-const connectDB = require("./server/database/dbConnection");
-const fileRouter = require("./server/routes/fileRoutes");
-const logger = require("./server/logger");
+const connectDB = require('./server/database/dbConnection');
+const fileRouter = require('./server/routes/fileRoutes');
+const logger = require('./server/logger');
 
 //Express App Init
 const app = express();
@@ -20,18 +21,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require("morgan")("tiny", { stream: logger.stream }));
+app.use(require('morgan')('tiny', { stream: logger.stream }));
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//load static files
+app.use('/css', express.static(path.resolve(__dirname, 'public/css/')));
+app.use('/img', express.static(path.resolve(__dirname, 'public/img/')));
 
 //API Routes
-
-app.use("/api", fileRouter);
+app.use('/api', fileRouter);
+app.use('/files', require('./server/routes/render'));
 
 //Express app listening
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   logger.log({
-    level: "info",
+    level: 'info',
     message: `Server running on port ${PORT} successfully..`,
   });
 });
