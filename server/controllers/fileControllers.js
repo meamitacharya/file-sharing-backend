2a52f20b152b394bc13cc88eab237654389d9557
Amit Acharya <amitacharya975@gmail.com>
2021-12-31 23:14:51 +0545

    :zap: sendEmail controller setup

--- server/controllers/fileControllers.js
+++ server/controllers/fileControllers.js
@@ -1,6 +1,8 @@
 const upload = require('../utils/upload');
 const File = require('../models/FileModel');
 const { v4: uuidV4 } = require('uuid');
+const sendEmail = require('../utils/emailSend');
+const emailTemplate = require('../utils/emailTemplate');
 
 exports.upload = (req, res) => {
   //store file
@@ -50,7 +52,7 @@ exports.show = async (req, res) => {
   if (!file) {
     return res.render('download', { error: 'Link has been expired' });
   }
-  console.log(file);
+//   console.log(file);
   return res.render('download', {
     uuid: file.uuid,
     fileName: file.fileName,
@@ -67,10 +69,9 @@ exports.download = async (req, res) => {
   if (!file) {
     return res.render('download', { error: 'Link has been expired' });
   }
-  console.log(file.path);
 
   const filePath = `${__dirname}/../../${file.path}`;
-  console.log(filePath);
+
   res.download(filePath);
 };
 
@@ -83,4 +84,31 @@ exports.send = async (req, res) => {
       message: `All fileds are required.`,
     });
   }
+
+  const file = await File.findOne({ uuid });
+  if (file.senderEmail) {
+    return res.status(422).json({
+      sucess: false,
+      message: `Email already sent.`,
+    });
+  }
+
+  file.receiverEmail = sendTo;
+  await file.save();
+  //  console.log(file)
+
+  const downloadLink = `${process.env.APP_BASE_URL}/files/${file.uuid}`;
+  const size = parseInt(file.size / 1000) + 'KB';
+
+  await sendEmail({
+    to: sendTo,
+    subject: `File sharing`,
+    text: 'Amit Shared a file with you.',
+    html: emailTemplate({ emailFrom: 'Amit', downloadLink, size, expires: '24 hours' }),
+  });
+
+  return res.status(200).json({
+    sucess: true,
+    message: 'Email Sent Sucessfully',
+  });
 };
